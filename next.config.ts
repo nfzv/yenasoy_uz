b6848bbc261903659369f2575210da37db70ee31
Nurbek <aidenscoding@gmail.com>
2026-03-07 22:10:01 +0200

    fix: update basePath and assetPrefix for production environment

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/yenasoy_uz" : "",
-  assetPrefix: isProd ? "/yenasoy_uz/" : "",
   images: {
     unoptimized: true,
   },
